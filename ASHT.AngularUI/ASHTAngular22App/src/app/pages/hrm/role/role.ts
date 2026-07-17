353319414703de27aa872c017eb6141056f101db
Ashraf Siddik <siddique_iceiu@yahoo.com>
2026-07-17 23:43:07 +0600

    role page modified

--- ASHT.AngularUI/ASHTAngular22App/src/app/pages/hrm/role/role.ts
+++ ASHT.AngularUI/ASHTAngular22App/src/app/pages/hrm/role/role.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-role',
@@ -6,4 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './role.html',
   styleUrl: './role.css',
 })
-export class Role {}
+export class Role {
+
+  roleForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.roleForm = this.fb.group({
+      roleName: ['', [Validators.required, Validators.minLength(3)]],
+      description: [''],
+      isActive: [true]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.roleForm.valid) {
+      console.log(this.roleForm.value);
+    }
+  }
+}
+
+
+
