593e9a0d7778695b8bc95a33560c5f6f22303d8f
Ashraf Siddik <siddique_iceiu@yahoo.com>
2026-07-20 16:26:57 +0600

    get all user VM

--- ASHT.Application/Hrm/User/Queries/GetAllUserQueries/GetAllUserQueryHandler.cs
+++ ASHT.Application/Hrm/User/Queries/GetAllUserQueries/GetAllUserQueryHandler.cs
@@ -13,7 +13,24 @@ namespace ASHT.Application.Hrm.User.Queries.GetAllUserQueries
 
         public async Task<List<UserVM>> Handle(GetAllUserQuery request, CancellationToken cancellationToken)
         {
-            return await _userService.GetAllUsersAsync(cancellationToken);
+            var users = await _userService.GetAllUsersAsync(cancellationToken);
+            return MapToUserVM(users);
+        }
+
+        private List<UserVM> MapToUserVM(List<Domain.Entities.Hrm.User> users)
+        {
+            return users.Select(user => new UserVM
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Password = user.Password,
+                PasswordSalt = user.PasswordSalt,
+                UserTypeId = user.UserTypeId,
+                IsActive = user.IsActive,
+                IsDeleted = user.IsDeleted,
+                CreatedDate = user.CreatedDate,
+                ModifiedDate = user.ModifiedDate
+            }).ToList();
         }
     }
 }
